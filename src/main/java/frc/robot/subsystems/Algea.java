a5587f4890c83bf13186682471b6fcd7bfe88794
Gilsa Media <120129694+GilsaMedia@users.noreply.github.com>
2025-01-19 16:43:19 +0200

    Update Algea.java

--- src/main/java/frc/robot/subsystems/Algea.java
+++ src/main/java/frc/robot/subsystems/Algea.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -9,9 +10,21 @@ import com.ctre.phoenix6.hardware.TalonFX;
 public class Algea extends SubsystemBase{
 
     private TalonFX m_motor;
+    private DigitalInput m_photoSwitch;
 
     public Algea() {
         m_motor = new TalonFX(Constants.AlgeaConstants.motorPort);
+        m_photoSwitch = new DigitalInput(Constants.AlgeaConstants.photoSwitchPort);
     }
+
+    public void move(double speed) {
+        m_motor.set(speed);
+    }
+
+    public Boolean isAlgeaIn() {
+        return m_photoSwitch.get();
+    }
+
+
 
 }
